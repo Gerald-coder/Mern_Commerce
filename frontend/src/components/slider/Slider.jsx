43bfefa7b68926df880dff9715d6e5f4772aa3ed
Oduigwe Gerald <paulsongerald00@gmail.com>
2023-11-13 11:58:57 +0100

    finised working on the slider component

--- frontend/src/components/slider/Slider.jsx
+++ frontend/src/components/slider/Slider.jsx
@@ -1,22 +1,81 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import { sliderData } from "./slider-data";
+import { useNavigate } from "react-router-dom";
+import "./Slider.scss";
 
 function Slider() {
   const [curSlide, setCurSlide] = useState(0);
-  const prevSlide = () => {};
-  const nextSlide = () => {};
+  const navigate = useNavigate();
+
+  const autoScroll = true;
+  const slideLength = sliderData.length;
+  let slideInterval;
+  const interval = 5000;
+
+  const prevSlide = () => {
+    setCurSlide((prev) => {
+      return prev === 0 ? (prev = sliderData.length - 1) : prev - 1;
+    });
+  };
+  const nextSlide = () => {
+    setCurSlide((prev) => {
+      return prev === sliderData.length - 1 ? (prev = 0) : prev + 1;
+    });
+  };
+
+  // setInterval(() => {
+  //    setCurSlide((prev) => {
+  //      return prev === sliderData.length - 1 ? (prev = 0) : prev + 1;
+  //    });
+  // }, interval)
+
+  useEffect(() => {
+    setCurSlide(0);
+  }, []);
+
+  useEffect(() => {
+    if (autoScroll) {
+      const auto = () => {
+        slideInterval = setInterval(nextSlide, interval);
+      };
+      auto();
+    }
+    return () => clearInterval(slideInterval);
+  }, [autoScroll, curSlide, interval]);
+
   return (
     <div className="slider">
-      <AiOutlineArrowLeft onClick={prevSlide} />
-      <AiOutlineArrowRight onClick={nextSlide} />
+      <AiOutlineArrowLeft onClick={prevSlide} className="arrow prev" />
+      <AiOutlineArrowRight onClick={nextSlide} className="arrow next" />
       {sliderData.map((slider, ind) => {
         const { desc, heading, image } = slider;
         return (
           <div
             key={ind}
             className={ind === curSlide ? "slide current" : "slide"}
-          ></div>
+          >
+            {ind === curSlide && (
+              <>
+                <img src={image} alt={desc} />
+                <div className="content">
+                  <span className="span1"></span>
+                  <span className="span2"></span>
+                  <span className="span3"></span>
+                  <span className="span4"></span>
+                  <h2>{heading}</h2>
+                  <p>{desc}</p>
+                  <hr />
+                  <button
+                    className="--btn --btn-primary"
+                    onClick={() => navigate("/shop")}
+                  >
+                    Buy now
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
         );
       })}
     </div>
--- frontend/src/components/slider/slider-data.jsx
+++ frontend/src/components/slider/slider-data.jsx
@@ -1,11 +1,16 @@
 export const sliderData = [
   {
-    image: "https://i.ibb.co/D1TvpVL/white-shoe.jpg",
+    image: "https://i.ibb.co/hL8PM1g/ciaga.jpg",
+    heading: "thietre Tools",
+    desc: "Up to 30% off on all onsale proucts.",
+  },
+  {
+    image: "https://i.ibb.co/1vrRQGk/nik2.jpg",
     heading: "Shoes Villa",
     desc: "Up to 30% off on all onsale proucts.",
   },
   {
-    image: "https://i.ibb.co/Bz5v6xs/gadget.jpg",
+    image: "https://i.ibb.co/7rPVqJP/nike.jpg",
     heading: "Hot Gadgets",
     desc: "Up to 30% off on all onsale proucts.",
   },
@@ -22,11 +27,6 @@ export const sliderData = [
   {
     image: "https://i.ibb.co/tYcmTKr/blue-shoe.jpg",
     heading: "Nike Sneakers",
-    desc: "Up to 30% off on all onsale proucts.",
-  },
-  {
-    image: "https://i.ibb.co/SrJyvC2/DR-JOHN.jpg",
-    heading: "thietre Tools",
     desc: "Up to 30% off on all onsale proucts.",
   },
 ];
