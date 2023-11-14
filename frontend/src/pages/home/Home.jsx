d85d52e2cd83c4fdf75f4c3128d4f2f67d075095
Oduigwe Gerald <paulsongerald00@gmail.com>
2023-11-15 00:34:11 +0100

    completed the homeInfo and worked product header

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import Slider from "../../components/slider/Slider";
 import HomeInfo from "../../components/homeInfo/HomeInfo";
 
+export function ProductHeading({ heading, btnText }) {
+  return (
+    <>
+      <div className="--flex-between">
+        <h2 className="--fw-thin">{heading}</h2>
+        <button className="--btn">{btnText}</button>
+      </div>
+      <div className="--hr"></div>
+    </>
+  );
+}
+
 function Home() {
   return (
     <>
@@ -9,6 +21,7 @@ function Home() {
       <section>
         <div className="container">
           <HomeInfo />
+          <ProductHeading heading={"Recent Products"} btnText={"Buy now>>>>"} />
         </div>
       </section>
     </>
