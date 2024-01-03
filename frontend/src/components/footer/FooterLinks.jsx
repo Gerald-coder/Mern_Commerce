23f2761c6ca4c56e06aea62018085c374436b5d8
Oduigwe Gerald <paulsongerald00@gmail.com>
2024-01-03 23:36:49 +0100

    worked on the footer section

--- frontend/src/components/footer/FooterLinks.jsx
+++ frontend/src/components/footer/FooterLinks.jsx
@@ -5,6 +5,8 @@ import { FaInstagram } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
+/* eslint-disable */
+
 export const Logo = (
   <div className="logo">
     <Link to="/">
@@ -14,6 +16,28 @@ export const Logo = (
     </Link>
   </div>
 );
+
+function NavLinks({ heading, one, two, three, four }) {
+  return (
+    <div className="footer-menu">
+      <p className="link-heading">{heading}</p>
+      <ul className="nav-ul footer-links">
+        <li>
+          <a href="">{one} </a>
+        </li>
+        <li>
+          <a href="">{two}</a>
+        </li>
+        <li>
+          <a href="">{three}</a>
+        </li>
+        <li>
+          <a href="">{four}</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
 function FooterLinks() {
   return (
     <>
@@ -34,6 +58,34 @@ function FooterLinks() {
       <section className="footer-section">
         <div className="container footer">
           <div className="footer-logo">{Logo}</div>
+          <NavLinks
+            heading={"Feature"}
+            one={"Link Shortening"}
+            two={"Branded Links"}
+            three={"Analytics"}
+            four={"Blog"}
+          />
+          <NavLinks
+            heading={"Resources"}
+            one={"Blog"}
+            two={"Developer"}
+            three={"Support"}
+            four={"Docs"}
+          />
+          <NavLinks
+            heading={"Companies"}
+            one={"About"}
+            two={"Our Team"}
+            three={"Carrier"}
+            four={"Contact"}
+          />
+          <NavLinks
+            heading={"Partners"}
+            one={"About"}
+            two={"Our Team"}
+            three={"Carrier"}
+            four={"Contact"}
+          />
         </div>
       </section>
     </>
