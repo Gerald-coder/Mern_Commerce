e5540e605ca21fb79fc28b6cb6c872df93560090
Oduigwe Gerald <paulsongerald00@gmail.com>
2024-02-03 00:09:09 +0100

    worked on the loader component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Loader from "./components/loader/Loader";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Loader />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
